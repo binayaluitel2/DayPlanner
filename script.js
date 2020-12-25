d74f8d24267560fd35fb81331e0e438ac47950f5
binayaluitel2 <binayababuluitel@gmail.com>
2020-12-25 11:11:24 -0500

    Adding Local Storage

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
   var today = moment().format("dddd, MMMM Do");
   $(".todaysDate").append(today);
+
+  $(document).ready(function () {
+    var today = moment().format("dddd, MMMM Do");
+    console.log(today);
+    $(".todaysDate").append(today);
+    var now = parseInt(moment().format("HH"));
+    var $9AM = $("#9AM");
+    $("button").on("click", function () {
+      localStorage.setItem("0", $9AM.val());
+    });
+
+    $("#9AM").append(localStorage.getItem("0"));
+  });
 });
