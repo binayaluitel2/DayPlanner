6bc39c89f9d5a505607b31e40e10961575cd6a17
binayaluitel2 <binayababuluitel@gmail.com>
2020-12-25 09:15:25 -0500

    Minor bug fixes and conversion into functions

--- script.js
+++ script.js
@@ -8,12 +8,24 @@ $(document).ready(function () {
   var $9AM = $("#9AM");
   var $10AM = $("#10AM");
   var $11AM = $("#11AM");
-  var $12AM = $("#12AM");
+  var $12PM = $("#12PM");
   var $1PM = $("#1PM");
   var $2PM = $("#2PM");
   var $3PM = $("#3PM");
   var $4PM = $("#4PM");
   var $5PM = $("#5PM");
+
+  var timeHour = [
+    $9AM,
+    $10AM,
+    $11AM,
+    $12PM,
+    $1PM,
+    $2PM,
+    $3PM,
+    $4PM,
+    $5PM
+  ];
 
   $("textarea").each(function () {
     var name = parseInt($(this).attr("name"));
@@ -42,24 +54,12 @@ $(document).ready(function () {
   });
 
   $("button").on("click", function () {
-    localStorage.setItem("0", $9AM.val());
-    localStorage.setItem("1", $10AM.val());
-    localStorage.setItem("2", $11AM.val());
-    localStorage.setItem("3", $12AM.val());
-    localStorage.setItem("4", $1PM.val());
-    localStorage.setItem("5", $2PM.val());
-    localStorage.setItem("6", $3PM.val());
-    localStorage.setItem("7", $4PM.val());
-    localStorage.setItem("8", $5PM.val());
+    for (i = 0; i < timeHour.length; i++) {
+      localStorage.setItem(i.toString(), timeHour[i].val());
+    }
   });
 
-  $("#9AM").append(localStorage.getItem("0"));
-  $("#10AM").append(localStorage.getItem("1"));
-  $("#11AM").append(localStorage.getItem("2"));
-  $("#12AM").append(localStorage.getItem("3"));
-  $("#1PM").append(localStorage.getItem("4"));
-  $("#2PM").append(localStorage.getItem("5"));
-  $("#3PM").append(localStorage.getItem("6"));
-  $("#4PM").append(localStorage.getItem("7"));
-  $("#5PM").append(localStorage.getItem("8"));
+  for (i = 0; i < timeHour.length; i++) {
+    timeHour[i].append(localStorage.getItem(i.toString()));
+  }
 });
