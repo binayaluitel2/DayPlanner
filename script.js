ba0d0088ee167bdad59ef33b94f2aa5b63dec2ff
binayaluitel2 <binayababuluitel@gmail.com>
2020-12-25 09:03:40 -0500

    Added past, present and future logic and style changes

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 $(document).ready(function () {
   var today = moment().format("dddd, MMMM Do");
+  console.log(today);
   $(".todaysDate").append(today);
-
-  $(document).ready(function () {
-    var today = moment().format("dddd, MMMM Do");
-    console.log(today);
-    $(".todaysDate").append(today);
-    var now = parseInt(moment().format("HH"));
-    var $9AM = $("#9AM");
-    $("button").on("click", function () {
-      localStorage.setItem("0", $9AM.val());
-    });
-
-    $("#9AM").append(localStorage.getItem("0"));
+  var now = parseInt(moment().format("HH"));
+  var $9AM = $("#9AM");
+  $("textarea").each(function () {
+    var name = parseInt($(this).attr("name"));
+    if (name < now) {
+      $(this).addClass("textarea-past");
+    }
+    if (name > now) {
+      $(this).addClass("textarea-future");
+    }
+    if (name === now) {
+      $(this).addClass("textarea-present");
+    }
   });
+  $("button").each(function () {
+    var name = parseInt($(this).attr("name"));
+    if (name < now) {
+      $(this).addClass("button-past");
+    }
+    if (name > now) {
+      $(this).addClass("button-future");
+    }
+    if (name === now) {
+      $(this).addClass("button-present");
+    }
+  });
+  $("button").on("click", function () {
+    localStorage.setItem("0", $9AM.val());
+  });
+  $("#9AM").append(localStorage.getItem("0"));
 });
